48a9526a5dc8c036498baefcce06d724089ff4e1
scbae <scbae27@gmail.com>
2025-05-14 18:00:22 +0900

    0514-04 절대경로 변경

--- src/daemon/webServer.js
+++ src/daemon/webServer.js
@@ -1,23 +1,35 @@
 import express from 'express';
 import session from 'express-session';
+import path from 'path';
+import { fileURLToPath } from 'url';
 class WebServer {
   constructor(id){
     this.id = id;
   }
   serviceOn(){
     const app = express();
+
+    // __dirname 설정 (ES 모듈 환경)
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    // views 경로를 절대경로로 설정
     app.set('view engine', 'pug');
-    app.set('views','./src/views');
+    app.set('views', path.join(__dirname, '../views'));
     app.locals.pretty = true;
-    app.use(express.static('./src/public'));
+
+    app.use(express.static(path.join(__dirname, '../public')));
+
     app.use(session({
       secret: 'your_secret_key',
       resave: false,
       saveUninitialized: false,
       cookie: { secure: false }
     }));
+
     return app;
   }
 }
+
 const webServer = new WebServer('webServer');
 export default webServer.serviceOn();
