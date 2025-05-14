49ea1b92bb6c59043e32dfd7f588d68b2dfa207d
scbae <scbae27@gmail.com>
2025-05-14 18:29:20 +0900

    0514-06

--- src/public/js/user/idCheck.js
+++ src/public/js/user/idCheck.js
@@ -12,6 +12,7 @@ document.querySelector('.id-btn').addEventListener('click', (e) => {
 function handleSubmit() {
     const idInput = document.getElementById('idInput').value;
     console.log('id:' + idInput);
+    
     fetch('/user/idCheck', {
         method: 'POST',
         headers: {
@@ -34,7 +35,6 @@ function handleSubmit() {
           confirmModal.showModalLink(data.message, false);
           // alert(data.message);
           console.error('아이디 있음:', data.message);
-          e.preventDefault();
           //로그인 실패 메시지를 어딘가에 표시하거나 적절히 처리
         }
       })
