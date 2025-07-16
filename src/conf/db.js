b4b616abdd832f321c9814d2ae60eb6a7be4c1bd
scbae <scbae27@gmail.com>
2025-07-16 14:31:35 +0900

    pool로 변경

--- src/conf/db.js
+++ src/conf/db.js
@@ -1,15 +1,23 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  charset: 'utf8mb4',
+  charset: "utf8mb4",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
+const db = {
+  query: (...args) => pool.query(...args),
+  pool,
+};
 
 export default db;
